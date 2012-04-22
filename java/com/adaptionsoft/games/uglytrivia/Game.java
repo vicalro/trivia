d8f4580540748de230c45bf60c26ef6f1bdf527a
victor <victor@victor-HP.(none)>
2012-04-22 21:09:56 +0200

    Eliminada la classe Singleton, considerat codi mort  després de l'analisi i del test de cobertura.

--- java/com/adaptionsoft/games/uglytrivia/Game.java
+++ java/com/adaptionsoft/games/uglytrivia/Game.java
@@ -177,20 +177,6 @@ public class Game {
 		return true;
 	}
 
-	public static class SimpleSingleton {
-    private static SimpleSingleton singleInstance =  new SimpleSingleton();
- 
-    //Marking default constructor private
-    //to avoid direct instantiation.
-    private SimpleSingleton() {
-    }
- 
-    //Get instance for class SimpleSingleton
-    public static SimpleSingleton getInstance() {
- 
-        return singleInstance;
-  }
-}
 	/**
 	 * Tells if the last player won.
 	 */
